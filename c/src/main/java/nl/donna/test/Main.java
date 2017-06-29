374cd87a23812c42c0999dc47d51c449b7ca7153
Jasper de Vries <jasper.devries@ns.nl>
2017-06-29 14:25:48 +0200

    Fix

---
+++ c/src/main/java/nl/donna/test/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2016 ProRail & Nederlandse Spoorwegen N.V.
+ *
+ * Broncode en het daarin voorkomende intellectueel eigendom
+ * behoren toe aan ProRail en Nederlandse Spoorwegen N.V.
+ * Gebruik en/of publicatie in welke (afgeleide) vorm en wijze dan ook
+ * zijn zonder voorafgaande schriftelijke toestemming niet toegestaan.
+ *
+ * Created 29 jun. 2017
+ */
+package nl.donna.test;
+
+/**
+ * @author Jasper de Vries
+ */
+public class Main {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
